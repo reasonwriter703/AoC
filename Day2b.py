e4cdc1e4ae0be50ebc7195954a0d043121ef2cce
ReasonWriter703 <reasonwriter703@gmail.com>
2024-01-19 11:47:25 -0800

    - Added set working directory and timer - Removed Draw class object - games do not need to be saved for later access - Changed RGB vars to dictionary - access keys dynamically instead of using match/case

--- Day2b.py
+++ Day2b.py
@@ -1,51 +1,22 @@
-
-file1 = open('Day2-input.txt', 'r')
-Lines = file1.readlines()
-
-#create class object
-class Draw:
-    def __init__(g, GameID, R, G, B):
-        g.GameID = GameID
-        g.R = R
-        g.G = G
-        g.B = B
-
-#create list of all draws
-Games = []
-
-# Strips the newline character
-for line in Lines:
-    thisGame = line[:line.find(':')]
-    line = line[len(thisGame)+1:]
-    thisGame = int(thisGame[5:])
-    red = 0
-    grn = 0
-    blu = 0
-    
-    line = line.strip()
-    line = line.split(';')
-    for d in line:
-
-        d = d.split(',')
-        for i in d:
-            i = i.strip()
-            i = i.split()
-            match i[1]:
-                case "red":
-                    if int(i[0]) > red:
-                        red = int(i[0])
-                case "green":
-                    if int(i[0]) > grn:
-                        grn = int(i[0])
-                case "blue":
-                    if int(i[0]) > blu:
-                        blu = int(i[0])
-        
-    #append class obj to list
-    Games.append(Draw(thisGame, red, grn, blu))
+import sys
+import os
+import time
+working= os.environ.get("WORKING_DIRECTORY",os.path.dirname(sys.argv[0]) + "/inputs")
+if len(sys.argv) > 1: working = sys.argv[1]
+os.chdir( working )
+start_time = time.time()
 
 PowerSum = 0
-for d in Games:
-    PowerSum += (d.R * d.G * d.B)
+for game in open('Day2-input.txt', 'r'):
+    game = game[game.find(':')+1:]
+    g = {'red':0, 'green':0, 'blue':0}
+
+    for draw in game.split(';'):
+        draw = draw.split(',')
+        for i in draw:
+            i = i.strip().split()
+            g[i[1]] = max(g[i[1]], int(i[0]))
+    PowerSum += (g['red'] * g['green'] * g['blue'])
 
 print("SUM OF GAME SETS: ", PowerSum)
+print('Time taken:', time.time() - start_time)
